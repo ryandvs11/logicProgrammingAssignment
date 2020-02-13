7e501f9e1e4212d9d3d9df554dab2b52f05dc834
ryandvs11 <32751317+ryandvs11@users.noreply.github.com>
2020-02-13 18:03:37 -0500

    Question #2 Started

--- csci350.pl
+++ csci350.pl
@@ -5,3 +5,11 @@ list-chars([X|Xs], N):-
     ;   list-chars(Xs, N0),
         succ(N0, N)
     ).
+    
+sum-up-numbers-simple([], 0).  % empty list
+
+sum-up-numbers-simple([L | LRest], Result) :- 
+	number(L),
+	sum-up-numbers-simple(LRest, LRestResult),
+	Result is L + LRestResult.
+
